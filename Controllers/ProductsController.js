74b2375daccf36e842923baee814f5bc5a5ef6a4
Chau Anh Kiet <kietcaps39286@gmail.com>
2024-11-05 03:02:28 +0700

    validate url api

--- Controllers/ProductsController.js
+++ Controllers/ProductsController.js
@@ -46,7 +46,7 @@ class TourController {
     async getOneProductQuery(req,res) {
         try {
             let query = req.query.t
-            query = convertString(validator.escape(query));
+            query = convertString(validator.whitelist(query,'a-zA-Z0-9'));
             const tour = await Tours.findOne({name:query});
             if(!tour) {
                 return res.status(404).json({
@@ -122,6 +122,7 @@ class TourController {
     }
 }
 
+   
 
 
 module.exports = new TourController();
