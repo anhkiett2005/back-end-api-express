1a68129822270ef5a54857984143817c245c2538
Chau Anh Kiet <kietcaps39286@gmail.com>
2024-11-13 06:33:33 +0700

    Update model Express

--- Models/Product.js
+++ Models/Product.js
@@ -21,6 +21,15 @@ const productSchema = new Schema(
             type: String,
             required: true
         },
+        location: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ['Tour', 'Sự kiện & Show diễn']
+        },
         category: {
             type: String,
             required: true,
@@ -36,6 +45,23 @@ const productSchema = new Schema(
             min: 1,
             max: 5,
             default: 3
+        },
+        duration: {
+            type: String,
+            required: function() {
+                return this.type === 'Tour';
+            },
+            validate: {
+                validator: function(value) {
+                    if (this.type === 'Tour') {
+                        return /^(\d+ ngày \d+ đêm)$/.test(value);
+                    } else if (this.type === 'Sự kiện & Show diễn') {
+                        return /^(\d+ giờ|\d+ ngày)$/.test(value);
+                    }
+                    return true;
+                },
+                message: props => `Giá trị ${props.value} không hợp lệ cho duration!`
+            }
         }
     },
     {
