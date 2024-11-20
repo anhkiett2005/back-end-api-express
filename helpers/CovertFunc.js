9b0de119881ccf267bfa7938bb9f0113ed048d90
Chau Anh Kiet <kietcaps39286@gmail.com>
2024-11-21 03:10:39 +0700

    custom helpers capitalizeWords string

--- helpers/CovertFunc.js
+++ helpers/CovertFunc.js
@@ -1,24 +1,9 @@
 function convertString(str) {
-    const regex = /[-_]/;
-      // tìm và xóa toàn bộ khoảng trắng
-      let words = str.replace(/\s+/g, '');
-
-      // Thay thế tất cả các dấu gạch ngang, dấu gạch dưới bằng khoảng trắng
-
-      if(regex.test(str)) {
-        words = words.replace(/[-_]/g, ' ');
-        
-        // Viết hoa chữ cái đầu của mỗi từ
-        return words.replace(/\b\w/g, char => char.toUpperCase());
-      }else {
-        const newWord =  words.replace(/(\w+?)(\d+)/, function(_, letters, numbers) {
-          return letters.charAt(0).toUpperCase() + letters.slice(1) + ' ' + numbers;
-      });
-
-
-        return newWord;
-      }
-  }
+  return str
+  .split(' ') // Tách chuỗi thành mảng các từ
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Viết hoa chữ cái đầu của mỗi từ
+  .join(' '); // Ghép mảng các từ thành chuỗi
+}
 
 
 module.exports = convertString;
